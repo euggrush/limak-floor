47d6152107fef5f7c9758eaf95134c1894ab25b8
Evgenii Grushin <evgenii.grushin@fortify.pro>
2023-01-11 14:17:49 -0800

    work on setup

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Navigation />
+  <router-view />
+  <Footer />
 </template>
+
+<script>
+import Navigation from "./components/Partials/Navigation.vue";
+import Footer from "./components/Partials/Footer.vue";
+export default {
+  components: {
+    Navigation,
+    Footer,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
