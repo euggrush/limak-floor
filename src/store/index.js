84dfbc9574639e5be610bff2d68e33a21a37142f
Evgenii Grushin <evgenii.grushin@fortify.pro>
2023-01-22 17:32:46 -0800

    work on request form

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex';
+import Axios from 'axios';
+
+import {
+  createLogger
+} from 'vuex';
+
+import {
+  CORPORATE_DATA
+} from '../constants';
+
 
 export default createStore({
   state: {
+    form_request: ``
   },
   mutations: {
+    SET_FORM_REQUEST(state, payload) {
+      state.form_request = payload;
+    },
   },
   actions: {
+    SEND_FORM_REQUEST: async (context, payload) => {
+      await Axios.post(`${CORPORATE_DATA.formSendUrl}`, payload).then((resp) => {
+        context.commit(`SET_FORM_REQUEST`, resp);
+      }).catch((error) => {
+        context.commit(`SET_FORM_REQUEST`, error);
+      })
+    },
   },
-  modules: {
-  }
+  modules: {},
+  plugins: [
+    createLogger(),
+  ],
 })
