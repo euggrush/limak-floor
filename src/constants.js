a0a548e90a3ef139d615d8525e904d5e659f44a4
Evgenii Grushin <evgenii.grushin@fortify.pro>
2023-01-31 19:20:08 -0800

    phone, google tag

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const CORPORATE_DATA = {
     instagrammLink: `https://www.instagram.com/limak_floor_services`,
     phoneNumbers: {
         roman: `+12368625225`,
-        anna: `+16474538824`
+        anna: `+14372595818`
     },
     address: `4 Beaver Bend Crescent, Etobicoke, ON M9B 5P8`,
     email: `service@limak-floor.ca`,
